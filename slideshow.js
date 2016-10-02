5f7a60c53bc3fa2ac68c1e8dbab8bc2a48e5e463
Andreas Bigger <abigger87@gmail.com>
2016-10-02 00:01:44 -0400

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -1,18 +1,18 @@
 var imgArray = [
-                  'images/cshpac.jpg',
-                  'images/ColdSpringHarborHighSchool.JPG',
-                  'images/csh.jpg'],
-                  curIndex = 0;
-                  imgDuration = 3000;
+    'http://placehold.it/300x200',
+    'http://placehold.it/200x100',
+    'http://placehold.it/400x300'],
+    curIndex = 0;
+    imgDuration = 3000;
 
-              function slideShow() {
-                  document.getElementById('imgGallary').className += "fadeOut";
-                  setTimeout(function() {
-                      document.getElementById('imgGallary').src = imgArray[curIndex];
-                      document.getElementById('imgGallary').className = "";
-                  },1000);
-                  curIndex++;
-                  if (curIndex == imgArray.length) { curIndex = 0; }
-                  setTimeout(slideShow, imgDuration);
-              }
-              slideShow();
+function slideShow() {
+    document.getElementById('slider').className += "fadeOut";
+    setTimeout(function() {
+        document.getElementById('slider').src = imgArray[curIndex];
+        document.getElementById('slider').className = "";
+    },1000);
+    curIndex++;
+    if (curIndex == imgArray.length) { curIndex = 0; }
+    setTimeout(slideShow, imgDuration);
+}
+slideShow();
