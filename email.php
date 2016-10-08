81343df5045508c473107b242ce08d5cbf6139a9
Andreas Bigger <abigger87@gmail.com>
2016-10-08 15:29:28 -0400

    Update email.php

--- email.php
+++ email.php
@@ -1,14 +1,19 @@
-<?php
-#strip slashes before putting the form data into target file
-$cd=stripslashes($_POST['email']);
- #Show the msg,if the code string is empty
- if(empty($cd))
- {echo "You didnt enter anything";}
-#if the code string is not empty then open the target file and put form data in it
-else
-{$file=fopen("textfile.txt","w");
-echo fwrite($file,$cd);
-#show a success msg 
+<?php 
+if(isset($_POST['submit'])){
+    $to = "cshschoolstore@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = "Newsletter submission";
+    $subject2 = "Copy of your Newsletter submission";
+    $message = $first_name . " " . $last_name . "" . $from . "\n\n";
+    $message2 = $first_name . " " . $last_name . "\n" . "Thank you for signing up for the CSH School Store Newsletter!";
 
-echo "Your email was succefully recorded";
-fclose($file);}?>
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Thank you for signing up!";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
