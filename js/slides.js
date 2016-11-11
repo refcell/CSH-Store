c9eb3e06f7faa88de0dd8853292b8a53acb16360
Andreas Bigger <abigger87@gmail.com>
2016-11-10 21:29:12 -0500

    Create slides.js

---
+++ js/slides.js
@@ -0,0 +1,11 @@
+$(function(){
+      // Hide all images except the first within your "fade In" <div>
+      $('.fadein img:gt(0)').hide();
+      // Set an interval to occur every 3 seconds (3000ms)
+      setInterval(function(){
+        // Fade out the first element and fade in the next and then move the elements
+        $('.fadein :first-child').fadeOut()
+           .next('img').fadeIn()
+           .end().appendTo('.fadein');}, 
+        6000);
+      });
