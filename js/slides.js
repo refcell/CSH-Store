53ee100cf7df3ccbd806153ecb1ccefb27aed24e
Andreas Bigger <abigger87@gmail.com>
2016-11-23 15:51:00 -0500

    Update slides.js

--- js/slides.js
+++ js/slides.js
@@ -1,6 +1,6 @@
 $(function(){
       // Hide all images except the first within your "fade In" <div>
-      $('.fadein img:gt(0)').hide();
+      $('.fadein img:gt(1)').hide();
       // Set an interval to occur every 3 seconds (3000ms)
       setInterval(function(){
         // Fade out the first element and fade in the next and then move the elements
