6935e2a6847ed2266be61d6eeeefa3decedb9452
Andreas Bigger <abigger87@gmail.com>
2016-12-03 17:25:10 -0500

    Update endsearch.js

--- js/endsearch.js
+++ js/endsearch.js
@@ -1,6 +1,8 @@
 //window.onload = function {
   var htmlfiles = window.location.hash.substring(1);
   var e = document.getElementById('search');
-  e.appendChild("<html-include src=" + searchproducts[x][1] + "></html-include>");
-  e.insertAdjacentHTML(beforeend, "<html-include src=" + searchproducts[x][1] + "></html-include>");
+  for(var x = 0; x < htmlfiles.length; x ++) {
+    e.appendChild("<html-include src=" + htmlfiles[x] + "></html-include>");
+    e.insertAdjacentHTML(beforeend, "<html-include src=" + htmlfiles[x] + "></html-include>");
+  }
 //}
