ac21bb103c3646fdbe1eb9020e0d5a02eb91bf1e
Andreas Bigger <abigger87@gmail.com>
2016-12-04 17:49:57 -0500

    Update endsearch.js

--- js/endsearch.js
+++ js/endsearch.js
@@ -1,8 +1,10 @@
 var html = JSON.parse(localStorage.getItem('html'));
 var test = false;
 console.log(html[0]);
+var noresults = document.createElement('div');
 var div = document.createElement('div');
 var finalstring = '';
+noresults.className = 'row';
 div.className = 'row';
 for(var x = 0; x < html.length; x++) {
   for(var y = x; y < html.length; y++) {
@@ -16,4 +18,11 @@ for(var x = 0; x < html.length; x++) {
   test = false;
 }
 div.innerHTML = finalstring;
-document.getElementById('search').appendChild(div);
+if(finalstring == "") {
+  finalstring = "<br  />" + "<br  />" + "<h1>No Products Found</h1>" + "<p>Try searching with a different search term or view our departments</p>;
+  noresults.innerHTML = finalstring;
+  document.getElementById('search').appendChild(noresults);
+}
+else {
+  document.getElementById('search').appendChild(div);
+}
