744b65977b11f875aeb7609d4bfa32489b4d9b22
Andreas Bigger <abigger87@gmail.com>
2016-12-03 20:22:59 -0500

    Update endsearch.js

--- js/endsearch.js
+++ js/endsearch.js
@@ -1,5 +1,6 @@
 //window.onload = function {
   var html = JSON.parse(localStorage.getItem('html'));
+  console.log(html[0]);
   var htmlfiles = window.location.hash.substring(1);
   var e = document.getElementById('search');
   for(var x = 0; x < html.length; x++) {
