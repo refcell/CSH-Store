ac3fa7b7a45f85548804053d99bcebab41b5a003
Andreas Bigger <abigger87@gmail.com>
2016-12-04 16:53:34 -0500

    Update endsearch.js

--- js/endsearch.js
+++ js/endsearch.js
@@ -1,10 +1,19 @@
 var html = JSON.parse(localStorage.getItem('html'));
+var test = false;
 console.log(html[0]);
 var div = document.createElement('div');
 var finalstring = '';
 div.className = 'row';
 for(var x = 0; x < html.length; x++) {
-finalstring = finalstring + "<html-include src=" + html[x] + "></html-include>";
+  for(var y = 0; y < html.length; y++) {
+    if((html[y] == html[x]) && (x != y)) {
+      test = true;
+    }
+  }
+  if(test == false){
+    finalstring = finalstring + "<html-include src=" + html[x] + "></html-include>";
+  }
+  test = false;
 }
 div.innerHTML = finalstring;
 document.getElementById('search').appendChild(div);
